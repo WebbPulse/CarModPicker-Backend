3fb11e0c9249cb48a19bd4727ad328e784e38530
Tyler <tylert2610@gmail.com>
2025-04-27 21:43:31 -0700

    placeholder models

--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,5 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
-from db.base_class import Base  # noqa
+from db.base_class import Base
 # Import your models here:
 from api.models.item import Item
